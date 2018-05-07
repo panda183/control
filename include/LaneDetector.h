32049ce913e9f94336eb70289372f5fb4e48d98c
DungTA <cucarrot97@gmail.com>
2018-05-07 13:57:38 +0700

    new lane detector

--- include/LaneDetector.h
+++ include/LaneDetector.h
@@ -1,26 +1,25 @@
 #ifndef LANE_DETECTOR_H
 #define LANE_DETECTOR_H
 
+#include <iostream>
 #include <opencv2/opencv.hpp>
-#include <vector>
-using namespace std;
+#include <cmath>
+
+#define RIGHT 1
+#define LEFT -1
+
 using namespace cv;
+using namespace std;
 
-#define NO_SIGN 0
-#define LEFT 1
-#define RIGHT 2
-#define STOP 3
-
-class LaneDetector
+namespace ld
 {
-public:
-    LaneDetector();
-    ~LaneDetector();
-    void inputImg(Mat img);
-    void findLane();
-    Point findLanePoint(int hug, Point start);
-private:
-    Mat img, src;
-};
-
+    extern int xCenterLane;
+    extern Vec3f laneCurve;
+    void findLane(Mat &lane,int laneFollow);
+    int avgX(Mat &window,int whitePixel);
+    Mat birdView(Mat &input);
+    Vec3f CurveEstimation(vector<Point2f> lanePoints);
+    void drawCurve(Mat &InputMat,Vec3f &curve);
+}
+	
 #endif
